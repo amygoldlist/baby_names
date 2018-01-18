681fb4fb5fe4ba7eaed55d3dfa03e343017ea1c5
Amy Goldlist <amygoldlist@gmail.com>
2018-01-18 15:22:08 -0800

    changes to cleaning script

--- src/clean_data.R
+++ src/clean_data.R
@@ -20,21 +20,31 @@ male_data <- read_csv("data/bc-popular-boys-names.csv")
 female_data <- read_csv("data/bc-popular-girls-names.csv")
 
 
-##Add in a column to indicate sex
+
+
+##Add in a column to indicate sex, and gather data
 female_data <- female_data %>% 
+  gather(key = "year", value = "n", 2:102) %>% 
   mutate(Sex = 0)
 
+female_data
+
 male_data <- male_data %>% 
+  gather(key = "year", value = "n", 2:102) %>% 
   mutate(Sex = 1)
 
 ## create one data.frame, with Sex as a factor
 names <- bind_rows(female_data, male_data) %>% 
   mutate(Sex =as.factor(Sex))
 
-names %>% 
-  select("Name", "Sex", "Total", everything())
+##clean up
+names <- names %>% 
+  filter(year !="Total") %>% 
+  mutate(year = as.integer(year))
+
+names
 
 
 
 ##write data to a new csv
-write_csv(names, "results/name_data.csv")
+write_csv(names, "app_files/app_data/name_data.csv")
