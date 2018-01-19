456eb7c609fc1d0cd67fd26673b9a694e8d40ad3
Amy Goldlist <amygoldlist@gmail.com>
2018-01-19 13:38:34 -0800

    changed cleanign to create 2 files

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -6,14 +6,13 @@
 
 
 ## This script takes the raw data from DataBC, and cleans it up, making it into one nice dataframe for analysis
+## This saves two versions:  the tidy one for plotting, the original to create a correlation, using years as features
 
 
 
 
-###load libraries
 library(tidyverse)
-#library(stringr)
-#library(forcats)
+
 
 ##read in data
 male_data <- read_csv("data/bc-popular-boys-names.csv")
@@ -22,27 +21,32 @@ female_data <- read_csv("data/bc-popular-girls-names.csv")
 
 ##Add in a column to indicate sex, and gather data
 female_data <- female_data %>% 
-  gather(key = "year", value = "n", 2:102) %>% 
+  #gather(key = "year", value = "n", 2:102) %>% 
   mutate(Sex = 0)
 
-#female_data
 
 male_data <- male_data %>% 
-  gather(key = "year", value = "n", 2:102) %>% 
+  #gather(key = "year", value = "n", 2:102) %>% 
   mutate(Sex = 1)
+
+
 
 ## create one data.frame, with Sex as a factor
 names <- bind_rows(female_data, male_data) %>% 
-  mutate(Sex =as.factor(Sex))
-
-##clean up
-names <- names %>% 
-  filter(year !="Total") %>% 
-  mutate(year = as.integer(year))
+  mutate(Sex =as.factor(Sex)) %>% 
+  select(-Total) %>% 
+  select(Name, Sex, everything())
 
 names
 
+##write data to a new csv, for correlations
+write_csv(names, "app_files/app_data/name_spread_data.csv")
 
 
-##write data to a new csv
-write_csv(names, "app_files/app_data/name_data.csv")
+##Gather the data for plotting, and clean data
+name_g <- names %>% 
+  gather(key="year", value = "n", 3:102) %>% 
+  mutate(year = as.integer(year))
+
+
+write_csv(name_g, "app_files/app_data/name_data.csv")
